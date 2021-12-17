c5575dfd344a8721269665b6213ed13f92fb5612
Mario D'Occhio <docchiomario@gmail.com>
2021-12-17 14:31:00 +0100

    mo

--- advent2021/day3part2/day3part2.cpp
+++ advent2021/day3part2/day3part2.cpp
@@ -51,26 +51,11 @@ int main()
 
 	while (getline(myfile, line))
 	{
-		if (((line.at(0) - 48) == binarray[0]) && (line.at(1) - 48 == binarray[1]) && (line.at(2) - 48 == binarray[2]) &&
-			(line.at(3) - 48 == binarray[3]) && (line.at(4) - 48 == binarray[4]) && (line.at(5) - 48 == binarray[5]) &&
-			(line.at(6) - 48 == binarray[6]) && (line.at(7) - 48 == binarray[7]) && (line.at(8) - 48 == binarray[8]) &&
-			(line.at(9) - 48 == binarray[9]) && (line.at(10) - 48 == binarray[10]) && (line.at(11) - 48 == binarray[11]))
+		if (line.at(0) - 48 == binarray[0] && oxygen < 2048)
 		{
-			cout << "\n" << "and the winner is me" << "\n";
-			for (i = 0; i < 12; i++)
-			{
-				if (binarray[i] == 1)
-				{
-					oxygen = oxygen + pow(2, k);
-					k--;
-				}
-				else if (binarray[i] == 0)
-				{
-					cotwo = cotwo + pow(2, k);
-					k--;
-				}
-			}
+			oxygen = oxygen + 2048
 		}
+     
 	}
 
 	cout << "oxygen is " << oxygen << " and co2 is " << cotwo << endl;
