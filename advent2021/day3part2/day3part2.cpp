83cfe4491e7c27cc9419c418fa5696d70832e489
Mario D'Occhio <docchiomario@gmail.com>
2021-12-17 14:10:49 +0100

    commit please

--- advent2021/day3part2/day3part2.cpp
+++ advent2021/day3part2/day3part2.cpp
@@ -14,8 +14,8 @@ int main()
 	int binarray[12]{};
 	int i{};
 
-	int gamma{};
-	int epsilon{};
+	int oxygen{};
+	int cotwo{};
 	int k{ 11 };
 
 	myfile.open("input.txt", ios::in);
@@ -41,14 +41,40 @@ int main()
 			binarray[i] = 0;
 	}
 
+	for (i = 0; i < 12; i++)
+	{
+		cout << binarray[i] << " ";
+	}
+
+	myfile.close();
+	myfile.open("input.txt", ios::in);
+
 	while (getline(myfile, line))
 	{
-		if ((line.at(0) == binarray[0])&& (line.at(1) == binarray[1])&& (line.at(2) == binarray[2])&& 
-			(line.at(3) == binarray[3])&& (line.at(4) == binarray[4])&& (line.at(5) == binarray[5])&& 
-			(line.at(6) == binarray[6])&& (line.at(7) == binarray[7])&& (line.at(8) == binarray[8])&& 
-			(line.at(9) == binarray[9])&& (line.at(10) == binarray[10])&& (line.at(11) == binarray[11]))
-			cout << "and the winner is me" << "\n"
+		if (((line.at(0) - 48) == binarray[0]) && (line.at(1) - 48 == binarray[1]) && (line.at(2) - 48 == binarray[2]) &&
+			(line.at(3) - 48 == binarray[3]) && (line.at(4) - 48 == binarray[4]) && (line.at(5) - 48 == binarray[5]) &&
+			(line.at(6) - 48 == binarray[6]) && (line.at(7) - 48 == binarray[7]) && (line.at(8) - 48 == binarray[8]) &&
+			(line.at(9) - 48 == binarray[9]) && (line.at(10) - 48 == binarray[10]) && (line.at(11) - 48 == binarray[11]))
+		{
+			cout << "\n" << "and the winner is me" << "\n";
+			for (i = 0; i < 12; i++)
+			{
+				if (binarray[i] == 1)
+				{
+					oxygen = oxygen + pow(2, k);
+					k--;
+				}
+				else if (binarray[i] == 0)
+				{
+					cotwo = cotwo + pow(2, k);
+					k--;
+				}
+			}
+		}
 	}
+
+	cout << "oxygen is " << oxygen << " and co2 is " << cotwo << endl;
+	cout << "life support rating is " << oxygen * cotwo;
 
 	myfile.close();
 
